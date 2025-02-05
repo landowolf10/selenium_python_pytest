014903fff44190718fafdd8ebc64632eb423af4d
Orlando Avila <orlando.avilag@hotmail.com>
2025-02-05 10:52:37 -0600

    Integrating with CircleCI.

--- src/utils/setup.py
+++ src/utils/setup.py
@@ -38,6 +38,9 @@ class SetUp:
             chrome_options: ChromeOptions = ChromeOptions()
             # chrome_options.add_argument("--remote-allow-origins=*")
             chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--disable-gpu")
             capabilities = chrome_options.to_capabilities()
         elif browser.lower() == "firefox":
             GeckoDriverManager().install()
